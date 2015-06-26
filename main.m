376cdd3c81885ab121de638d3a070c314689eaef
iBook <pgyxfirst@icloude.com>
2015-06-26 11:02:27 +0800

    commit2

---
+++ main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  LessonCategory
+//
+//  Created by iBook on 15/5/11.
+//  Copyright (c) 2015年 李海建. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSDate+dateFromOneString.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+    
+        // @"20140402142850"
+        [NSDate dateFromOneString:@"20140402142850"];
+    
+
+    }
+    return 0;
+}
